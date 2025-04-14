0ad39e6ac4321b7261dbf17f7c13cd47ccbcc55d
Dave Kennedy <dave-kennedy@users.noreply.github.com>
2025-04-13 20:56:27 -0600

    Disentangle header, remove logo and background

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,8 +1,6 @@
-import React, { useRef, useState , useEffect} from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 
 import '../App.css';
-import logo from '../Images/Logos/bustoshow-text-logo--white-outline-no-fill-328x46.png'
 import { useStore } from '../Store';
 
 const Header = (props) => {
@@ -11,53 +9,27 @@ const Header = (props) => {
   const navigate = useNavigate();
   const location = useLocation();
 
-  const [showDashboardButton, toggleShowDashboardButton] = useState(true);
-  const myRef = useRef(null);
-
-  useEffect(() => {
-    window.$(myRef.current).tooltip();
-    if(!btsUser.isLoggedIn) toggleShowDashboardButton(false)
-  }, []);
-
-  const navToDashboard = () => {
-    navigate('/', { state: { from: '/login'} })
-  }
+  const handleClick = (event) => {
+    if (location.pathname === '/') {
+      navigate('/login');
+    } else {
+      navigate('/');
+    }
+  };
 
   return (
-
-      <nav className={props.adminView ? 'Header row bts-admin-purple nav-flex' : 'Header row bts-orange-header align-items-center nav-flex'}>
-        <div className="col-sm-2 ml-3 mt-1 mr-4">
-          <img src={logo} width="180" height="24" className="" alt="bts-logo" />
-        </div>
-        <div className="col-sm-2">
-          <div
-            onClick={location.pathname !== '/login' ? ()=>{navigate('/login')}: ()=>{navToDashboard()}}
-            className={props.adminView ? "border-0 bts-admin-purple p-2 mr-2" : "border-0 bts-orange-bg p-2 mr-2"}>
-            <div>
-                <button className="btn detail-btn mr-2" onClick={()=>{toggleShowDashboardButton(!showDashboardButton)}}>
-                  { !showDashboardButton
-                  ?
-                    !btsUser.isLoggedIn
-                    ?
-                      <div ref={myRef} data-toggle="tooltip" data-placement="bottom" title="Sign in to view your reservations, history and more.">
-                        Sign in/up
-                      </div>
-                    :
-                      <div>
-                        <i className="fas fa-user-check fa-lg"></i>
-                      </div>
-                  :
-                  <div>
-                    Dashboard
-                  </div>
-                  }
-                </button>
-            </div>
-      </div>
-    </div>
+    <nav className='Header pb-4'>
+      <button className="btn detail-btn mr-2" onClick={handleClick}>
+        {
+          location.pathname === '/'
+            ? btsUser.isLoggedIn
+              ? 'Dashboard'
+              : 'Sign In/Up'
+            : 'Back to Events'
+        }
+      </button>
     </nav>
-
-  )
+  );
 }
 
 export default Header;
